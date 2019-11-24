d9c0c20b4ba53a7e5df2693191b23b821fc1528a
Sy Nguyen <tansyab1@users.noreply.github.com>
2019-11-24 14:35:30 +0100

    Update index.js

--- tp3/js/index.js
+++ tp3/js/index.js
@@ -1,23 +1,21 @@
-$(document).ready(function () {
-	$("button").click(function () {
-		/* votre code ici*/
-		if ($("#nom").val().length < 1 || $("#prenom").val().length < 1 || $("#datepicker").val().length < 1 || $("#addr").val().length < 1 || $("#mail").val().length < 1) {
-			$('#lModalLabel').text('Error');
-			$('#modal1').text('Veuillez remplir tous les champs svp');
-			$('#lModal').modal('show');
-		}
-		else {
-			$('#lModalLabel').text('Bienvenue ' + $("#nom").val());
-			$("#modal1").text('Vous êtes nés le' + $("#datepicker").val() + ' et vous habitez');
-			$("#lModal .modal-body a").attr("href", "https://maps.google.com/maps?q=" + $("#addr").val() + "&markers=" + $("#addr").val());
-			$("#lModal .modal-body a img").attr("src", "https://maps.googleapis.com/maps/api/staticmap?size=400x250&center=" + $("#addr").val() + "&markers=" + $("#addr").val());
-			$("#modal3").text($("#addr").val());
-			$('#lModal').modal('show');
-		}
+$(document).ready(function(){
+    $("button").click(function(){
+        if (($("#nom").val().length==0) || ($("#prenom").val().length==0) || ($("#naissance").val().length==0 || ($("#adresse").val().length==0) || ($("#email").val().length==0)) ) {
+            $('#Modal1').modal('show');
+        }
+        else {
+            $('#myModalLabel2').text('Bienvenue ' + $("#nom").val());
+            $('#modalP1').text('Vous êtes nés le ' + $("#naissance").val() +' et vous habitez');
+            $('#modalP3').text($("#adresse").val());  
+			$("#Modal2 .modal-body a").attr("href","https://maps.google.com/maps?q="+$("#adresse").val()+"&markers="+$("#adresse").val());
+            $("#Modal2 .modal-body a img").attr("src", "https://maps.googleapis.com/maps/api/staticmap?&center="+$("#adresse").val()+"&markers="+$("#adresse").val()+"&zoom=12&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg");
+            $('#Modal2').modal('show');
+        }
+    });
+
+    $( "#naissance" ).datepicker({
+		dateFormat: "dd/mm/yy",
+		maxDate: 0
 	});
 
-	$("#datepicker").datepicker({
-		dateFormat: "dd/mm/yy",
-		maxDate: 31
-	});
 });
