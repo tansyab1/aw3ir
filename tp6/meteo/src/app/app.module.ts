b5f7fc264ba555e04637047ed0408a5e59b89054
Sy Nguyen <tansyab1@users.noreply.github.com>
2020-02-10 00:00:03 +0100

    Update app.module.ts

--- tp6/meteo/src/app/app.module.ts
+++ tp6/meteo/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+import { FormsModule } from  '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
